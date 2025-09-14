ac1fdb25c9ddd4db53bae008c2ded001a367232d
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-09-14 21:15:21 +0600

    little modified

--- routes/api.php
+++ routes/api.php
@@ -9,54 +9,60 @@ use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\WalletController;
 use Illuminate\Support\Facades\Route;
 
-// Route::controller(AuthController::class)->group(function () {
-//     Route::post('register', 'register');
-//     Route::post('login', 'login');
-// });
 
 Route::prefix('v1')->group(function () {
 
-
-    Route::get('/user/dashboard', [UserController::class, 'dashboard'])->middleware('auth:sanctum');
-    Route::get('/agent/dashboard', [UserController::class, 'AgentDashboard'])->middleware('auth:sanctum'); // new
-    Route::get('/admin/dashboard', [UserController::class, 'AdminDashboard'])->middleware('auth:sanctum'); // new
-
-
-    Route::prefix('admin')->group( function () {
-        // Route::get('/dashboard', [UserController::class, 'AgentDashboard'])->middleware('auth:sanctum'); // new
-        Route::get('/transactions', [TransactionController::class, 'adminTransaction'])->middleware('auth:sanctum'); // new
-        // Route::get('/listing', [TransactionController::class, 'adminListing'])->middleware('auth:sanctum'); // new
-        Route::get('/listings', [TransactionController::class, 'adminListing']); // new
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('/auth/login', 'login');
+        Route::post('/auth/logout', 'logout')->middleware('auth:sanctum');
     });
 
 
-    Route::get('/user/all-users', [UserController::class, 'index'])->middleware('auth:sanctum', 'role:ADMIN');
-    Route::patch('/user/{id}/status', [UserController::class, 'statusChange'])->middleware('auth:sanctum', 'role:ADMIN');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('admin')->group( function () {
+            Route::get('/dashboard', [UserController::class, 'AdminDashboard']); // new
+            Route::get('/transactions', [TransactionController::class, 'adminTransaction']); // new
+            Route::get('/listings', [TransactionController::class, 'adminListing']); // new
+        });
+    });
 
+    Route::prefix('user')->group( function () {
+        Route::post('/register', [UserController::class, 'register']);
 
-    Route::get('/user/profile', [UserController::class, 'profile'])->middleware('auth:sanctum'); //new
-    Route::put('/user/profile', [UserController::class, 'profileUpdate'])->middleware('auth:sanctum'); //new
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/dashboard', [UserController::class, 'dashboard']);
+            Route::get('/all-users', [UserController::class, 'index'])->middleware('role:ADMIN');
+            Route::patch('/{id}/status', [UserController::class, 'statusChange'])->middleware('role:ADMIN');
+            Route::get('/profile', [UserController::class, 'profile']); //new
+            Route::put('/profile', [UserController::class, 'profileUpdate']); //new
+        });
+    });
 
+    Route::prefix('agent')->group( function () {
+        Route::get('/dashboard', [UserController::class, 'AgentDashboard'])->middleware('auth:sanctum'); // new
+
+    });
+
+    // Route::get('/user/all-users', [UserController::class, 'index'])->middleware('auth:sanctum', 'role:ADMIN');
+    // Route::patch('/user/{id}/status', [UserController::class, 'statusChange'])->middleware('auth:sanctum', 'role:ADMIN');
+    // Route::get('/user/profile', [UserController::class, 'profile'])->middleware('auth:sanctum'); //new
+    // Route::put('/user/profile', [UserController::class, 'profileUpdate'])->middleware('auth:sanctum'); //new
 
 
     Route::get('/agents', [AgentController::class, 'getAllAgents'])->middleware('auth:sanctum', 'role:ADMIN');
     Route::patch('/agents/{id}/status', [AgentController::class, 'statusChange'])->middleware('auth:sanctum', 'role:ADMIN'); //depricated
 
 
-
-    Route::post('/user/register', [UserController::class, 'register']);
-
-
-    Route::prefix('wallet')->group( function () {
-        //User
-        Route::patch('/add-money', [WalletController::class, 'addMoney'])->middleware('auth:sanctum');
-        Route::patch('/withdraw-money', [WalletController::class, 'withdrawMoney'])->middleware('auth:sanctum');
-        Route::post('/send-money', [WalletController::class, 'sendMoney'])->middleware('auth:sanctum');
-
-        // Agent
-        Route::post('/cash-in', [WalletController::class, 'cashIn'])->middleware('auth:sanctum');
-        Route::post('/cash-out', [WalletController::class, 'cashOut'])->middleware('auth:sanctum');
-
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('wallet')->group( function () {
+            //User
+            Route::patch('/add-money', [WalletController::class, 'addMoney'])->middleware('auth:sanctum');
+            Route::patch('/withdraw-money', [WalletController::class, 'withdrawMoney'])->middleware('auth:sanctum');
+            Route::post('/send-money', [WalletController::class, 'sendMoney'])->middleware('auth:sanctum');
+            // Agent
+            Route::post('/cash-in', [WalletController::class, 'cashIn'])->middleware('auth:sanctum');
+            Route::post('/cash-out', [WalletController::class, 'cashOut'])->middleware('auth:sanctum');
+        });
     });
 
     Route::prefix('transaction')->group( function () {
@@ -64,35 +70,4 @@ Route::prefix('v1')->group(function () {
     });
 
 
-
-
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/auth/login', 'login');
-        Route::post('/auth/logout', 'logout')->middleware('auth:sanctum');
-    });
 });
-
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::post('/logout', [AuthController::class, 'logout']);
-
-//     Route::middleware(['role:admin'])->group(function () {
-//         Route::prefix('services')->group(function () {
-//             Route::controller(ServiceController::class)->group(function () {
-//                 Route::post('/', 'store');
-//                 Route::put('/{id}', 'update');
-//                 Route::delete('/{id}', 'destroy');
-//             });
-//         });
-//         Route::get('/admin/bookings', [BookingController::class, 'getAllBookingsByAdmin']);
-//     });
-
-//     Route::middleware(['role:customer'])->group(function () {
-//         Route::get('/services', [ServiceController::class, 'index']);
-//         Route::prefix('bookings')->group(function () {
-//             Route::get('/', [BookingController::class, 'getAllBookingsByCustomer']);
-//             Route::post('/', [BookingController::class, 'store']);
-//         });
-//     });
-
-// });
